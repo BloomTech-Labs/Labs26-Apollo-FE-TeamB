99d5a9079151e9b8af8f39ddf55a0e62a88ccde6
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-09-08 15:51:27 -0700

    removed test links and examples from home render page

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -6,30 +6,9 @@ import Navigation from './Nav';
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
-    <div>
+    <>
       <Navigation />
-      <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
-      <div>
-        <p>
-          This is an example of a common example of how we'd like for you to
-          approach components.
-        </p>
-        <p>
-          <Link to="/profile-list">Profiles Example</Link>
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
-        </p>
-        <p>
-          <Link to="/datavis">Data Visualizations Example</Link>
-        </p>
-        <p>
-          <Button type="primary" onClick={() => authService.logout()}>
-            Logout
-          </Button>
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
 export default RenderHomePage;
