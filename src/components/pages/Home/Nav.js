fd3a0831961e207927364196e9ff84aaa103b3c0
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-09-08 12:51:24 -0700

    created Nav component file in home component, built basic code structure in react

---
+++ src/components/pages/Home/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <nav>
+      <h1>Apollo</h1>
+      <a href="">New Topic</a>
+      <a href="">Join Topic</a>
+      <a href="">Signout</a>
+    </nav>
+  );
+}
+
+export default Navigation;
