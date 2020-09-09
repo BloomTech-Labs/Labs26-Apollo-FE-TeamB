72c19dd8d4361ab37b00d128cf8d475158b20bdf
Justin Lohner <lohner4@gmail.com>
2020-09-09 14:40:21 -0600

    refactored form again, took out form.list and changed it to map through questions passed in and create elements for them and added add question button

--- src/components/common/QuestionForm.js
+++ src/components/common/QuestionForm.js
@@ -1,67 +1,35 @@
 import React, { useState, useEffect, createRef } from 'react';
 import { Form, Input, Button } from 'antd';
-// import FormInput from './FormInput';
 import { MinusCircleOutlined } from '@ant-design/icons';
 
-// const formItemLayout = {
-//   labelCol: {
-//     xs: { span: 24 },
-//     sm: { span: 4 },
-//   },
-//   wrapperCol: {
-//     xs: { span: 24 },
-//     sm: { span: 20 },
-//   },
-// };
-//
-// const formItemLayoutWithOutLabel = {
-//   wrapperCol: {
-//     xs: { span: 24, offset: 0 },
-//     sm: { span: 20, offset: 4 },
-//   },
-// };
-
 const QuestionForm = ({ defaultQuestions }) => {
-  // const ref1 = createRef()
+  const [questions, setQuestions] = useState(defaultQuestions);
+  const handleClick = updateQuestions => {
+    setQuestions(updateQuestions);
+  };
   return (
     <Form name="question-form">
-      <Form.List name="questions">
-        {(fields, { add, remove }) => {
-          if (fields == 0) {
-            defaultQuestions.forEach(q => {
-              add(q);
-            });
-          }
-          return (
-            <div>
-              {fields.map((field, index) => (
-                <Form.Item key={field.key} label={`Question ${index + 1}`}>
-                  <Input placeholder="question" style={{ width: '60%' }} />
-                  {fields.length > 1 ? (
-                    <MinusCircleOutlined
-                      style={{ margin: '0 8px' }}
-                      onClick={() => {
-                        remove(field.name);
-                      }}
-                    />
-                  ) : null}
-                </Form.Item>
-              ))}
-              <Form.Item>
-                <Button
-                  type="primary"
-                  onClick={() => {
-                    add();
-                  }}
-                  style={{ width: '60%' }}
-                >
-                  add
-                </Button>
-              </Form.Item>
-            </div>
-          );
-        }}
-      </Form.List>
+      {questions.map((question, index) => (
+        <Form.Item key={index} label={`Question ${index + 1}`}>
+          <Input placeholder={question} style={{ width: '60%' }} />
+          <MinusCircleOutlined
+            style={{ margin: '0 8px' }}
+            onClick={() => {
+              handleClick(questions.filter((testQ, i) => i !== index));
+            }}
+          />
+        </Form.Item>
+      ))}
+      <Form.Item>
+        <Button
+          type="primary"
+          onClick={() => {
+            handleClick([...questions, 'New Question']);
+          }}
+        >
+          add
+        </Button>
+      </Form.Item>
     </Form>
   );
 };
