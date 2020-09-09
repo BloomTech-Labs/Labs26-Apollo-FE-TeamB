7d9b8b9e9d2e5f7080119822ad6d7ff3b6e5d0e5
Ian McElroy <mcelroyian@gmail.com>
2020-09-08 20:29:57 -0500

    added proposed global state

--- src/components/pages/TopicCreation/TopicCreation.js
+++ src/components/pages/TopicCreation/TopicCreation.js
@@ -4,6 +4,225 @@ import { Step1, Step2, Step3, Step4, Step5 } from './Steps/';
 
 import 'antd/dist/antd.css';
 
+const dummyState = {
+  user: {
+    userid: 5,
+    username: 'Mark',
+  },
+  contextTopics: [
+    {
+      id: 1,
+      name: 'Software Manager',
+      leaderQuestions: [
+        {
+          id: 1,
+          type: 'text',
+          body: 'What is the priorty for the week',
+        },
+        {
+          id: 2,
+          type: 'text',
+          body: 'What are our hard deadlines?',
+        },
+        {
+          id: 3,
+          type: 'text',
+          body: 'Is there any new information the team needs?',
+        },
+      ],
+      memberQuestions: [
+        {
+          id: 4,
+          type: 'text',
+          body: 'Do you have any blockers',
+        },
+        {
+          id: 5,
+          type: 'text',
+          body: 'What task are you working on?',
+        },
+        {
+          id: 6,
+          type: 'text',
+          body: 'Will you be able to meet the hard deadlines?',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Design Manager',
+      leaderQuestions: [
+        {
+          id: 1,
+          type: 'text',
+          body: 'What is the priorty for the week',
+        },
+        {
+          id: 2,
+          type: 'text',
+          body: 'What are our hard deadlines?',
+        },
+        {
+          id: 3,
+          type: 'text',
+          body: 'Is there any new information the team needs?',
+        },
+      ],
+      memberQuestions: [
+        {
+          id: 4,
+          type: 'text',
+          body: 'Do you have any blockers',
+        },
+        {
+          id: 5,
+          type: 'text',
+          body: 'What task are you working on?',
+        },
+        {
+          id: 6,
+          type: 'text',
+          body: 'Will you be able to meet the hard deadlines?',
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Product Owner',
+      leaderQuestions: [
+        {
+          id: 1,
+          type: 'text',
+          body: 'What is the priorty for the week',
+        },
+        {
+          id: 2,
+          type: 'text',
+          body: 'What are our hard deadlines?',
+        },
+        {
+          id: 3,
+          type: 'text',
+          body: 'Is there any new information the team needs?',
+        },
+      ],
+      memberQuestions: [
+        {
+          id: 4,
+          type: 'text',
+          body: 'Do you have any blockers',
+        },
+        {
+          id: 5,
+          type: 'text',
+          body: 'What task are you working on?',
+        },
+        {
+          id: 6,
+          type: 'text',
+          body: 'Will you be able to meet the hard deadlines?',
+        },
+      ],
+    },
+  ],
+  userTopics: [
+    {
+      id: 4,
+      name: 'Labs 26 Standup',
+      leaderQuestions: [
+        {
+          id: 1,
+          type: 'text',
+          body: 'What is the priorty for the week',
+        },
+        {
+          id: 2,
+          type: 'text',
+          body: 'What are our hard deadlines?',
+        },
+        {
+          id: 3,
+          type: 'text',
+          body: 'Is there any new information the team needs?',
+        },
+      ],
+      memberQuestions: [
+        {
+          id: 4,
+          type: 'text',
+          body: 'Do you have any blockers',
+        },
+        {
+          id: 5,
+          type: 'text',
+          body: 'What task are you working on?',
+        },
+        {
+          id: 6,
+          type: 'text',
+          body: 'Will you be able to meet the hard deadlines?',
+        },
+      ],
+      members: [
+        { id: 1, name: 'Joe' },
+        { id: 2, name: 'Ian' },
+        { id: 3, name: 'Nick' },
+        { id: 4, name: 'Steven' },
+        { id: 5, name: 'Mark' },
+      ],
+      defaultSurvey: 17,
+      frequency: 'weekly',
+      ownerid: 5,
+    },
+    {
+      id: 5,
+      name: 'Leadership Standup',
+      leaderQuestions: [
+        {
+          id: 1,
+          type: 'text',
+          body: 'What is the priorty for the week',
+        },
+        {
+          id: 2,
+          type: 'text',
+          body: 'What are our hard deadlines?',
+        },
+        {
+          id: 3,
+          type: 'text',
+          body: 'Is there any new information the team needs?',
+        },
+      ],
+      memberQuestions: [
+        {
+          id: 4,
+          type: 'text',
+          body: 'Do you have any blockers',
+        },
+        {
+          id: 5,
+          type: 'text',
+          body: 'What task are you working on?',
+        },
+        {
+          id: 6,
+          type: 'text',
+          body: 'Will you be able to meet the hard deadlines?',
+        },
+      ],
+      members: [
+        { id: 14, name: 'Austen' },
+        { id: 7, name: 'Josh' },
+        { id: 5, name: 'Mark' },
+      ],
+      defaultSurvey: 37,
+      frequency: 'daily',
+      ownerid: 14,
+    },
+  ],
+};
+
 //how many steps the wizard has
 const lastStep = 5;
 
