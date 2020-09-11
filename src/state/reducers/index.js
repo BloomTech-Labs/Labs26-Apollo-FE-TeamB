3edc2851426eb776ba8ff657b22689c5c4804ca8
caleb <bargercj20@hotmail.com>
2020-09-11 15:04:55 -0700

    filled in an example indexjs file that would go inside of reducers using combineReducers from the redux lib

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,4 +1,12 @@
-// import all of your reducers into this file, and export them back out. 
+// import all of your reducers into this file, and export them back out.
 // This allows for the simplification of flow when importing reducers into your actions throughout your app.
 
+import { combineReducers } from 'redux';
 
+// example
+// --------------------------------------
+// import {fooReducer as foo} from './fooReducer'
+
+// const reducer = combineReducers({foo});
+
+// export default reducer
