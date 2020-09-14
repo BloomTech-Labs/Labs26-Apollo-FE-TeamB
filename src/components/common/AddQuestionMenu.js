215663e5de4cbefaec8467fe43f7b83cd02f43e2
Justin Lohner <lohner4@gmail.com>
2020-09-14 13:24:08 -0600

    finished AddQuestionMenu component

--- src/components/common/AddQuestionMenu.js
+++ src/components/common/AddQuestionMenu.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Form, Select } from 'antd';
 
-const AddQuestionMenu = ({ questionList, handleQuestionState }) => {
-  return <div></div>;
+// const place = "Add Context Question"
+// const place1 = "Add Question"
+
+// const dropdownStyle = {
+//   textAlign: "center",
+//   width: "40%"
+// }
+
+const layout = {
+  size: 'middle',
+  style: {
+    textAlign: 'center',
+    width: '50%',
+  },
+};
+
+const AddQuestionMenu = ({ isContext, questionList, stateHandler }) => {
+  const handleSelect = question => {
+    stateHandler(question);
+  };
+  return (
+    <Select
+      {...layout}
+      value={isContext ? 'Add Context Question' : 'Add Question'}
+      onSelect={handleSelect}
+    >
+      <Select.Option key={-1} value="Custom">
+        Custom
+      </Select.Option>
+      {questionList.map((question, idx) => (
+        <Select.Option key={idx} value={question}>
+          {question}
+        </Select.Option>
+      ))}
+    </Select>
+  );
 };
 
 export default AddQuestionMenu;
