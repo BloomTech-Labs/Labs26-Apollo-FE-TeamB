57cf71fc2a9782daee608f5d14fd0ab30929fcb8
Justin Lohner <lohner4@gmail.com>
2020-09-22 16:12:20 -0600

    working on state management with code field, also added individual input fields for each code letter

--- src/components/pages/JoinTopic/components/JoinCodeForm.js
+++ src/components/pages/JoinTopic/components/JoinCodeForm.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import { Form, Input } from 'antd';
+import React, { useState, createRef } from 'react';
+import { Form, Input, Button } from 'antd';
 
 const JoinCodeForm = () => {
   const [form] = Form.useForm();
+  const [code, setCode] = useState('');
+  const ref1 = createRef(null);
+  const ref2 = createRef(null);
+  const ref3 = createRef(null);
+  const ref4 = createRef(null);
+  const [refObj, setRefObj] = useState({
+    1: ref1,
+    2: ref2,
+    3: ref3,
+    4: ref4,
+  });
+  let x = 1;
+  const onInputChange = e => {
+    // setCode(code + e.target.value);
+    if (x < Object.keys(refObj).length) {
+      refObj[x + 1].current.focus();
+      x++;
+    } else if (x === 4) {
+      x = 1;
+    }
+  };
 
   return (
     <Form
@@ -15,7 +36,31 @@ const JoinCodeForm = () => {
         name="joinCode"
         // label='Join Topic'
       >
-        <Input
+        {[1, 2, 3, 4].map(i => {
+          // const currentRef = createRef(null);
+          let currentRef = null;
+          if (i === 1) {
+            currentRef = refObj[1];
+          } else if (i === 2) {
+            currentRef = refObj[2];
+          } else if (i === 3) {
+            currentRef = refObj[3];
+          } else if (i === 4) {
+            currentRef = refObj[4];
+          }
+          console.log(currentRef);
+          return (
+            <Input
+              // ref={ref`${i}`}
+              ref={currentRef}
+              key={`codeInput${i}`}
+              maxLength={1}
+              onChange={onInputChange}
+              style={{ width: '20%' }}
+            />
+          );
+        })}
+        {/* <Input
           // size='large'
           style={{
             fontSize: '3rem',
@@ -24,7 +69,7 @@ const JoinCodeForm = () => {
           }}
           // placeholder='Enter Topic Join Code'
           placeholder="Topic Code"
-        />
+        /> */}
       </Form.Item>
     </Form>
   );
