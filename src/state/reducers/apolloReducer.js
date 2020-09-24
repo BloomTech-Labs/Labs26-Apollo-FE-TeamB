09df3d994f5c0ed488804ee1cbfba1b1ada24e33
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-09-23 22:05:07 -0700

    removed userid from global state, created gettopic case in reducer

--- src/state/reducers/apolloReducer.js
+++ src/state/reducers/apolloReducer.js
@@ -1,11 +1,14 @@
 // import all of your reducers into this file, and export them back out.
 // This allows for the simplification of flow when importing reducers into your actions throughout your app.
 
-import { GET_USERNAME, GET_BEARER_TOKEN } from '../actions/apolloActions';
+import {
+  GET_USERNAME,
+  GET_BEARER_TOKEN,
+  GET_TOPICS,
+} from '../actions/apolloActions';
 
 const initialState = {
   bearerToken: '',
-  userid: '',
   username: '',
   titles: [],
 };
@@ -23,6 +26,11 @@ export const apolloReducer = (state = initialState, action) => {
       return {
         ...state,
         bearerToken: action.payload,
+      };
+    case GET_TOPICS:
+      return {
+        ...state,
+        topics: action.payload,
       };
     default:
       return state;
