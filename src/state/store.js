c77e99c5302135940d254c79c30e40730be9390b
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-09-29 13:34:37 -0700

    removed logger from store

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { apolloReducer } from './reducers/apolloReducer';
 
-export const store = createStore(apolloReducer, applyMiddleware(thunk, logger));
+export const store = createStore(apolloReducer, applyMiddleware(thunk));
 
 export default store;
