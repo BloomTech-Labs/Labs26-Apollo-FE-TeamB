4a6b7c7f54c120745a40497b8a35a803d90b479e
Caleb Barger <bargercj20@hotmail.com>
2020-10-09 08:36:17 -0700

    added some humor to the 404 page

--- src/components/pages/NotFound/NotFoundPage.js
+++ src/components/pages/NotFound/NotFoundPage.js
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
-    <div>
+    <>
       <h1>404 Page Not Found</h1>
       <Link to="/">
         <button>Back To Home</button>
       </Link>
-    </div>
+      <div>
+        <img
+          alt="gentle 404"
+          src="https://www.distilled.net/wp-content/themes/distilled_2011/images/all/404-gentlemen.jpg"
+        />
+      </div>
+    </>
   );
 };
 
