bdbed17c527114e28f4c9583c674361f30d2b32e
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-10-13 17:55:28 -0700

    created a TopicNav component to extract out the Topic nav, set bg color to indigo and set up icons to load from a list of ant Icons, used plus Icon

---
+++ src/components/pages/TopicNav/TopicNav.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Sider from 'antd/lib/layout/Sider';
+import {
+  ApartmentOutlined,
+  NodeIndexOutlined,
+  MobileOutlined,
+  FundProjectionScreenOutlined,
+  BugOutlined,
+  AccountBookOutlined,
+  AudioOutlined,
+  CarOutlined,
+  PlusCircleOutlined,
+} from '@ant-design/icons';
+
+function TopicNav(props) {
+  const topicLinkStyle = {
+    color: 'white',
+    border: '4px solid white',
+    borderRadius: '50%',
+    padding: '1rem',
+    fontSize: '2rem',
+  };
+  const icons = [
+    <ApartmentOutlined style={topicLinkStyle} />,
+    <NodeIndexOutlined style={topicLinkStyle} />,
+    <MobileOutlined style={topicLinkStyle} />,
+    <FundProjectionScreenOutlined style={topicLinkStyle} />,
+    <BugOutlined style={topicLinkStyle} />,
+    <AccountBookOutlined style={topicLinkStyle} />,
+    <AudioOutlined style={topicLinkStyle} />,
+    <CarOutlined style={topicLinkStyle} />,
+  ];
+
+  return (
+    <Sider
+      style={{
+        backgroundColor: 'indigo',
+        borderTopRightRadius: '4rem',
+        borderBottomRightRadius: '4rem',
+      }}
+    >
+      {props.topics.map((topic, index) => {
+        return (
+          <span
+            style={{ display: 'block', width: '100%', margin: '2rem 0' }}
+            onClick={() => console.log(topic)}
+          >
+            {icons[index]}
+          </span>
+        );
+      })}
+      <PlusCircleOutlined
+        onClick={() => console.log('This is happening!')}
+        style={{ fontSize: '4rem', color: 'white' }}
+      />
+    </Sider>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    ...state,
+    topics: state.topics,
+  };
+};
+
+export default connect(mapStateToProps, {})(TopicNav);
