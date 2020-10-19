150754e261d79eaf1ff2b981e86bd96098a0a688
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-10-18 22:20:27 -0700

    created Profile folder and component

---
+++ src/components/pages/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Modal } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+function Profile() {
+  return (
+    <div>
+      <UserOutlined />
+      <Modal></Modal>
+    </div>
+  );
+}
+
+export default Profile;
